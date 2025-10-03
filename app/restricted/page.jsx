e2a4ced6cef918fca684944c36ce3b46fc0d382b
Afridi-haque7 <afridi.hq97@gmail.com>
2025-10-04 00:40:17 +0530

    minor change

--- app/restricted/page.jsx
+++ app/restricted/page.jsx
@@ -7,7 +7,7 @@ function Restricted() {
           <div className=" flex flex-col gap-12 justify-center items-center">
             <h1 className="text-red-500 text-4xl font-bold ">Restricted</h1>
             <div className="text-white flex flex-col gap-4 justify-center items-center bg-white/10 px-8 py-12 rounded-lg">
-              <p className="text-xl font-semibold">
+              <p className="text-xl text-white font-semibold">
                 Sorry, You are not authrized to access this page 😕
               </p>
               <p className="text-xl font-semibold">Please SignIn first</p>
